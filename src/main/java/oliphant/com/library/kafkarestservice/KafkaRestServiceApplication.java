e551b9c75b33123c5b798f48d4afba242746b63c
Joshua Oliphant <josholiphantlmp@gmail.com>
2017-09-24 09:36:34 -0700

    Initial commit

---
+++ src/main/java/oliphant/com/library/kafkarestservice/KafkaRestServiceApplication.java
@@ -0,0 +1,12 @@
+package oliphant.com.library.kafkarestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/oliphant/com/library/kafkarestservice/KafkaRestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package oliphant.com.library.kafkarestservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaRestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
