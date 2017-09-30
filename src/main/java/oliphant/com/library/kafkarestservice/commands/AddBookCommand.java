235aaf68a157790572301afe5e2e25bb2ca1afde
Joshua Oliphant <josholiphantlmp@gmail.com>
2017-09-29 22:52:23 -0700

    Added commands to recieve, with correlationId

--- src/main/java/oliphant/com/library/kafkarestservice/commands/AddBookCommand.java
+++ src/main/java/oliphant/com/library/kafkarestservice/commands/AddBookCommand.java
@@ -2,12 +2,12 @@ package oliphant.com.library.kafkarestservice.commands;
 
 public class AddBookCommand implements Command {
 
-    //TODO: add correlationId
-
+    private String correlationId;
     private String authorFirstName;
     private String authorLastName;
 
-    public AddBookCommand(String authorFirstName, String authorLastName) {
+    public AddBookCommand(String authorFirstName, String authorLastName, String correlationId) {
+        this.correlationId = correlationId;
         this.authorFirstName = authorFirstName;
         this.authorLastName = authorLastName;
     }
